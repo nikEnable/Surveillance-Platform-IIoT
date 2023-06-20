acc067962965fea01313516a5135a1124f7abb91
nikEnable <94482318+nikEnable@users.noreply.github.com>
2023-06-21 02:42:40 +0300

    Update config.h

--- ARDUINO/config.h
+++ ARDUINO/config.h
@@ -11,5 +11,8 @@ const string ID = "nikEnable";
 const string Broker = "test.mosquitto.org";
 const string ClientName = ID + "IIoT_client";
 const int TCP_port = 1883;
-const string Client_pub_topic =  "iiotnode/flamedetection";
-const string Client_sub_topic =  "testtopic/condition";
+const string Client_pub_topic_flame =  "iiotnode/flamedetection";
+const string Client_pub_topic_temp =  "iiotnode/DHT11/temp";
+const string Client_pub_topic_hum =  "iiotnode/DHT11/hum";
+const string Client_sub_topic =  "iiotnode/condition";
+
